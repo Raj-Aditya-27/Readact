0e405c2ad9b79ef6a3420ffba51c5edc170a879a
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-08-30 03:25:16 +0530

    body section added

--- readact/src/components/Body.jsx
+++ readact/src/components/Body.jsx
@@ -3,10 +3,21 @@ import React from "react";
 function Body() {
   return (
     <div className="body">
-      <div className="heading">Redactify: Your Data Protector</div>
+      <div className="heading">Readact: Your Data Protector</div>
       <div className="files">
         <div className="inside-files">
-            <input type="file" name="file" id="file" />
+          <div className="input">
+            <div className="icons">
+              <i className="bi bi-file-earmark-image"></i>
+              <i className="bi bi-arrow-right-short"></i>
+              <i className="bi bi-filetype-pdf"></i>
+            </div>
+            {/* <input type="file" name="file" id="file" /> */}
+            <input type="file" id="file-input" class="file-input" />
+            <label for="file-input" class="file-input-label">
+              CHOOSE FILES
+            </label>
+          </div>
         </div>
       </div>
       <div className="scale">
